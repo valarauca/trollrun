1ac86cd5e94ff1def389fc1db143dc7022c6b56b
William Laeder <codylaeder@gmail.com>
2019-12-27 09:39:56 -0800

    csv stuff should be wrapping up
    TODO:
    * re-organize exec
    * create cli

--- src/marshal/csv/config.rs
+++ src/marshal/csv/config.rs
@@ -1,6 +1,7 @@
-use std::io::Write;
+use std::fs::OpenOptions;
+use std::io::{stdout, Write};
 
-use super::super::super::csv::{QuoteStyle, Writer, WriterBuilder};
+use super::super::super::csv::{QuoteStyle, Terminator, Writer, WriterBuilder};
 use super::super::super::serde::Deserialize;
 
 use super::BuildCSVOutput;
@@ -10,17 +11,40 @@ pub struct CSVConfig {
     pub path: Option<String>,
     pub seperator: Option<char>,
     pub quote: Option<char>,
-    pub buffer: Option<usize>,
     #[serde(default)]
     pub eol: Option<EOLSpecification>,
 }
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct EOLSpecifiation {
+pub struct EOLSpecification {
     pub lf: Option<bool>,
     pub cr: Option<bool>,
-    pub lfcr: Option<bool>,
-    pub specify: Option<char>,
+    pub crlf: Option<bool>,
+    pub specify: Option<u8>,
+}
+
+fn get_eol_specification(eol: &Option<EOLSpecification>) -> Terminator {
+    let eol = match eol {
+        &Option::None => return Terminator::CRLF,
+        &Option::Some(ref eol) => eol,
+    };
+    match &eol.lf {
+        &Option::Some(true) => return Terminator::Any(10),
+        _ => {}
+    };
+    match &eol.cr {
+        &Option::Some(true) => return Terminator::Any(13),
+        _ => {}
+    };
+    match &eol.crlf {
+        &Option::Some(true) => return Terminator::CRLF,
+        _ => {}
+    };
+    match &eol.specify {
+        &Option::Some(val) => return Terminator::Any(val),
+        _ => {}
+    };
+    Terminator::Any(13)
 }
 
 // construct the CSV output
@@ -34,7 +58,7 @@ impl BuildCSVOutput for CSVConfig {
 
         // set up field seperator
         match &self.seperator {
-            Option::Some(ref c) if c.is_ascii() => {
+            &Option::Some(ref c) if c.is_ascii() => {
                 w.delimiter(*c as u32 as u8);
             }
             _ => {
@@ -45,7 +69,7 @@ impl BuildCSVOutput for CSVConfig {
 
         // set up optional wrapping
         match &self.quote {
-            Option::Some(ref c) if c.is_ascii() => {
+            &Option::Some(ref c) if c.is_ascii() => {
                 w.quote(*c as u32 as u8);
                 // if you force a quote style it'll always be used.
                 w.quote_style(QuoteStyle::Always);
@@ -56,6 +80,25 @@ impl BuildCSVOutput for CSVConfig {
             }
         };
 
-        // TODO return the output
+        // figure out how to terminator lines
+        w.terminator(get_eol_specification(&self.eol));
+
+        // determine where to write to
+        let output: Box<dyn Write> = match &self.path {
+            &Option::Some(ref path) => {
+                match OpenOptions::new()
+                    .read(false)
+                    .write(true)
+                    .create(true)
+                    .truncate(true)
+                    .open(path)
+                {
+                    Err(e) => panic!("could not open file:'{}' error:'{:?}'", path, e),
+                    Ok(file) => Box::new(file),
+                }
+            }
+            _ => Box::new(stdout()),
+        };
+        w.from_writer(output)
     }
 }
