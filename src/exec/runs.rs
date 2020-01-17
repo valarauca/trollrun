d0d629c036a41fd51058a74c8d4f5e58f2b54dac
cody laeder <codylaeder@gmail.com>
2020-01-16 21:32:08 -0800

    update runs to preserve errors.
    I am screwing with mosml internals, and I need to see those errors.

--- src/exec/runs.rs
+++ src/exec/runs.rs
@@ -22,8 +22,8 @@ impl TrollOutput {
                 eprintln!("{} failed to exec with error:'{:?}'", self.name, e);
                 None
             }
-            RunResult::Error(s) => {
-                eprintln!("{} failed to exec with error:'{:?}'", self.name, s);
+            RunResult::Error(stdout, stderr) => {
+                eprintln!("{} failed.\n{}\n{}\n", self.name, stdout, stderr);
                 None
             }
             RunResult::Success(data) => Some(TrollRecordable {
@@ -52,22 +52,22 @@ impl TrollRecordable {
 /// RunResult contains the output of the orginal data.
 pub enum RunResult {
     ExecErr(Error),
-    Error(String),
+    Error(String, String),
     Success(TrollData),
 }
-impl From<&'static str> for RunResult {
-    fn from(arg: &'static str) -> RunResult {
-        RunResult::Error(arg.into())
+impl From<Error> for RunResult {
+    fn from(arg: Error) -> Self {
+        Self::ExecErr(arg)
     }
 }
-impl From<String> for RunResult {
-    fn from(arg: String) -> RunResult {
-        RunResult::Error(arg)
+impl From<&'static str> for RunResult {
+    fn from(arg: &'static str) -> Self {
+        Self::Error(arg.into(), "".into())
     }
 }
 impl<'a> From<&'a [u8]> for RunResult {
-    fn from(arg: &'a [u8]) -> RunResult {
-        RunResult::from(to_string(arg))
+    fn from(arg: &'a [u8]) -> Self {
+        Self::Error(to_string(arg), "".into())
     }
 }
 impl RunResult {
@@ -75,22 +75,22 @@ impl RunResult {
     fn new(arg: Command) -> RunResult {
         let mut arg = arg;
         let output = match arg.output() {
-            Err(err) => return RunResult::ExecErr(err),
+            Err(err) => return RunResult::from(err),
             Ok(output) => output,
         };
 
         // did the command succeed or fail?
         // troll doesn't use return codes, because of course not
-        let lines: TrollData = match (output.stdout.len(), output.stderr.len()) {
+        let (stdout, stderr) = match (output.stdout.len(), output.stderr.len()) {
             (0, 0) => return RunResult::from("no stdout/stderr returned from execution"),
-            (_, 0) => String::from_utf8_lossy(output.stdout.as_slice())
-                .lines()
-                .filter_map(TrollLine::new)
-                .collect(),
-            _ => return RunResult::from(output.stderr.as_slice()),
+            (_, _) => (
+                String::from_utf8_lossy(output.stdout.as_slice()).to_string(),
+                String::from_utf8_lossy(output.stderr.as_slice()).to_string(),
+            ),
         };
+        let lines: TrollData = stdout.lines().filter_map(TrollLine::new).collect();
         if lines.is_empty() {
-            RunResult::from("valid data was returned, but parsing failed")
+            RunResult::Error(stdout, stderr)
         } else {
             RunResult::Success(lines)
         }
